70846c6e6990106ca6c7aaeb101e42a1efe6d123
Anagi <nageswari.avula@gmail.com>
2023-08-22 01:37:17 +0530

    hellow world script added

--- 06-array.sh
+++ 06-array.sh
@@ -3,4 +3,7 @@
 # this is called array, instead of single value it can hold multiple values
 PERSONS=("Ramesh" "Suresh" "Sachin")
 # inside array we always refer values with index and it starts from 0
+
 echo "first person:${PERSONS[0]}"
+
+echo "first person:${PERSONS[@]}"
