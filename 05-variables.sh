a822093e4387690eed361ae35ef83f1a892b038b
Anagi <nageswari.avula@gmail.com>
2023-08-22 01:35:56 +0530

    hellow world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,7 @@ echo "please enter your user name"
 
 read -s USERNAME # this is the variable where the value of the one user enter
 
+#this is only for vaidation, we should not print in the terminal
 echo "Username entered is : $USERNAME"
 
 echo "please enter your user Password"
