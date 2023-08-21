f890b95722e723aeadf8a0d578026c748296c614
Anagi <nageswari.avula@gmail.com>
2023-08-22 00:33:03 +0530

    hellow world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -4,7 +4,7 @@ echo "Hello World ,I am learning DevOps with AWS"
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo"$PERSON1: Hi $PERSON2 ,Good Morning"
-echo"$PERSON2: Good Morning $PERSON1"
-echo"$PERSON1: How are you $PERSON2"
-echo"$PERSON2: i am good, how are you $PERSON1"
+echo "$PERSON1: Hi $PERSON2 ,Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
