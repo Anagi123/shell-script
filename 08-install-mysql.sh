95e5dbf74ffdc2e2c3b60333199908cde0aa341a
Anagi <nageswari.avula@gmail.com>
2023-08-22 21:27:04 +0530

    installation of pstfix

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -24,12 +24,12 @@ else
     echo "Installation of mysql is success"
 fi
 
-yum install pstfix -y
+yum install postfix -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installation of pstfix is error"
+    echo "Installation of postfix is error"
     exit 1
 else
-    echo "Installation of pstfix is success"
+    echo "Installation of postfix is success"
 fi
