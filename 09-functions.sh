1e47ac1685f232eed2bcbd96b88dfedbe438ff8a
Anagi <nageswari.avula@gmail.com>
2023-08-23 14:28:55 +0530

    functions added

--- 09-functions.sh
+++ 09-functions.sh
@@ -16,7 +16,6 @@ else
 }
 
 if [ $USERID -ne 0 ]
-
 then 
     echo "ERROR::Please run this script with root access"
     exit 1
@@ -27,24 +26,8 @@ fi
 # it is our responsibility again to check installation is success or not
 yum install mysql -y
 
-VALIDATE(){ 
-    
-}
-
-if [ $? -ne 0 ]
-then 
-    echo "Installation of mysql is error"
-    exit 1
-else
-    echo "Installation of mysql is success"
-fi
+VALIDATE()
 
 yum install postfix -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of postfix is error"
-    exit 1
-else
-    echo "Installation of postfix is success"
-fi
+VALIDATE()
