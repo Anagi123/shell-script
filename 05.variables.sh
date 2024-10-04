12af93332301321e622975371869743e701ae478
Anagi <nageswari.avula@gmail.com>
2024-10-05 00:29:59 +0530

    1st commit

--- 05.variables.sh
+++ 05.variables.sh
@@ -4,6 +4,7 @@ echo "please enter your username"
 
 read -s USERNAME # this is the variable where the value of the one user entered
 
+#this is only for validation, we should not print in the terminal
 echo "unsername entered is :$USERNAME"
 
 echo "please enter your Password"
