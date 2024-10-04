6c01edb420b7fab541c751436621200332f99281
Anagi <nageswari.avula@gmail.com>
2024-10-04 23:57:59 +0530

    1st commit

---
+++ 03.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello world i am learning devops with aws"
+PERSON1=$1 # this is the 1st value from command line
+PERSON2=$2 # this is the 2nd value from command line
+
+
+echo "$PERSON1:Hi $PERSON2, Good Morning"
+echo "$PERSON2:Good Morning $PERSON1"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:i am good , how are you $PERSON1"
