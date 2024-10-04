7a1d9651ec443305a36d56dbdf5a49b4c250bbb2
Anagi <nageswari.avula@gmail.com>
2024-10-04 23:35:54 +0530

    1st commit

--- 01.hello-world.sh
+++ 01.hello-world.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo"hello world i am learning devops with aws"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo"$PERSON1:Hi $PERSON2, Good Morning"
+echo"$PERSON2:Good Morning $PERSON1"
+echo"$PERSON1:How are you $PERSON2"
+echo"$PERSON2:i am good , how are you $PERSON1"
