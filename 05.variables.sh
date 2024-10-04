c348cbacfcc2d8ec3735361f849c30663ac21bf8
Anagi <nageswari.avula@gmail.com>
2024-10-05 00:17:01 +0530

    1st commit

--- 05.variables.sh
+++ 05.variables.sh
@@ -2,12 +2,12 @@
 
 echo "please enter your username"
 
-read USERNAME # this is the variable where the value of the one user entered
+read -s USERNAME # this is the variable where the value of the one user entered
 
 echo "unsername entered is :$USERNAME"
 
 echo "please enter your Password"
 
-read PASSWORD # this is the variable where the value of the one user entered
+read -s PASSWORD # this is the variable where the value of the one user entered
 
 echo "unsername entered is :$PASSWORD"
