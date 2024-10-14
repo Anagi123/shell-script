5ad978be4a207bfa1591207715dfab0eedc0d3d2
Anagi <nageswari.avula@gmail.com>
2024-10-14 19:55:58 +0530

    postfix installation

--- 09.functions.sh
+++ 09.functions.sh
@@ -2,7 +2,10 @@
 
 # our program goal is to install mysql
 
-USERID=$(id -u)
+DATE=$(date+%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 # this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
     #$1 --> it will receive the argument1
@@ -14,6 +17,9 @@ else
     echo "$2 ... SUCCESS"
 fi
 }
+
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:please run this script with root access"
@@ -23,11 +29,10 @@ then
 fi     
 
 # it is our responsibility again to check installation is success or not
-yum install mysql -y
-
+yum install mysql -y &>>$LOGFILE
 VALIDATE $? "installing mysql"
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "installing postfix"
 
